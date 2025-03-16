3fb3fc8eeb8387fde9277bd45997fd4e69e2aca0
Chrisad21 <50429171+Chrisad21@users.noreply.github.com>
2025-03-16 04:05:31 +0200

    Update

---
+++ Tutorials - Workflows/JOYCAPTION/JoyCaption-START.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Activate the virtual environment
+call venv\Scripts\activate
+
+:: Run the Python script
+python caption_gui.py
---
+++ Tutorials - Workflows/JOYCAPTION/README.txt
@@ -0,0 +1,2 @@
+#ΒΑΛΤΕ ΚΑΙ ΤΑ 2 ΑΡΧΕΙΑ ΣΤΟΝ ΦΑΚΕΛΟ joy-caption-alpha-two
+ΜΕ ΤΟ JoyCaption-START.bat ΞΕΚΙΝΑΜΕ ΤΟ ΠΡΟΓΡΑΜΜΑ.
